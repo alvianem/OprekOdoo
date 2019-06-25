d8ea86b8bb71d06e34de9a2eee9e725e9c7a16dc
Alvian Eka Maulana <alvian.em169@gmail.com>
2019-06-25 15:01:30 +0700

    punya alvian

---
+++ odoo/addons/customer_tag_group/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "customer_tag_group",
+
+    'summary': """
+        A demo showing you how to add quick customer tag group""",
+
+    'description': """
+        A demo showing you how to add quick customer tag group
+    """,
+
+    'author': "Alvian Eka Maulana",
+    'website': "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/partner_search.xml',
+    ]
+}
